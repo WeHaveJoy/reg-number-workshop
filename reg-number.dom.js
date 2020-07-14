b55a7268ef9edd00073dcfcdd8e466a1768f4187
WeHaveJoy <cnovuyo10@gmail.com>
2020-07-14 15:05:02 +0200

    new changes

--- reg-number.dom.js
+++ reg-number.dom.js
@@ -4,12 +4,42 @@ const regNumbers = document.querySelector(".regNumbers");
 const addRegNumberBtn = document.querySelector(".addRegNumber");
 const message = document.querySelector(".message");
 
+const regNumbersEntered = [];
+
 addRegNumberBtn.addEventListener("click", function() {
 
 	if (regNumberEntered.value == "") {
 		message.innerHTML = "Please enter a registration number";
 		return;
 	}
+	const currentRegNumber = regNumberEntered.value.toUpperCase();
 
-	regNumber.innerHTML = regNumberEntered.value;
+if( currentRegNumber.startsWith("CA") ||
+currentRegNumber.startsWith("CJ") ||
+currentRegNumber.startsWith("CY")){
+
+message.innerHTML = "";
+
+	
+	if(!regNumbersEntered.includes(currentRegNumber)){
+		
+	
+
+const regNumberElem = document.createElement("div");
+regNumberElem.classList.add("regNumber");
+regNumberElem.innerHTML = currentRegNumber;
+regNumbers.appendChild(regNumberElem);
+
+	//regNumber.innerHTML = regNumberEntered.value;
+
+	regNumbersEntered.push(currentRegNumber);
+	}else{
+		message.innerHTML = "This registration number already exists.";
+	}
+
+}else{
+	message.innerHTML = "Invalid registration number entered.";
+}
 });
+
+
